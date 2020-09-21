beb24877ecb1b994eea3ce6baa525573be5f015a
Sulton <iskhakov1995@mail.ru>
2020-09-21 12:40:48 +0500

    last changes are made

--- pkg/bank/stats/example_test.go
+++ pkg/bank/stats/example_test.go
@@ -24,3 +24,35 @@ func ExampleAvg() {
 	//Output:
 	//1500000
 }
+
+
+func ExampleTotalInCategory() {
+	payment:=[]types.Payment{
+		{
+			ID: 1000,
+			Amount: 10_000_00,
+			Category: "gold",
+		},
+		{
+			ID: 2000,
+			Amount: 20_000_00,
+			Category: "gold",
+		},
+		{
+			ID: 2000,
+			Amount: 30_000_00,
+			Category: "silver",
+		},
+		{
+			ID: 2000,
+			Amount: 40_000_00,
+			Category: "silver",
+		},
+			
+	}
+	a:="silver"
+	max:=TotalInCategory(payment,types.Category(a))
+	fmt.Println(max)
+	//Output:
+	//7000000
+}
